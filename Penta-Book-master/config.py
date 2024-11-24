eaadccecdc69107407c710d4ef7879255c345831
Dina Febriana <81072166+dinaFebriana24@users.noreply.github.com>
2024-11-24 16:03:44 +0700

    Delete Penta-Book-master directory

--- Penta-Book-master/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
-    DATABASE = os.getenv('DATABASE', 'penta_book.db')
-    DEBUG = os.getenv('DEBUG', 'false').lower() in ['true', '1', 't', 'y', 'yes']
